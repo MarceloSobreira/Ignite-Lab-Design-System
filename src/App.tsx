1c2d02f255f7d6607167245b32f8308bf78f1740
Marcelo S <marcelo15011@gmail.com>
2022-10-12 20:06:47 -0300

    Projeto de Design System desenvolvido no IgniteLab

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css'
+
+
+export function App() {
+  return (
+    <>
+      <h1 className="text-2xl">Hello World!</h1>
+      <button className="p-4 rounded text-xl font-bold bg-cyan-500 text-gray-900 hover:bg-cyan-300">Click</button>
+    </>
+  )
+}
